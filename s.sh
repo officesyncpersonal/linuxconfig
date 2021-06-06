cacb5b6241f1dfd00fcc5f53d7d3f4fc76ba416d
dhirajdusane <dhirajdusane@hotmail.com>
2021-06-06 22:43:52 +0530

    kafka starting scripts

--- s.sh
+++ s.sh
@@ -7,5 +7,10 @@ export lconfig=$tmp/linuxconfig
 export khome=$tmp/kafka
 export kconfig=$tmp/kafkaconfig
 
+alias kzs='nohup $khome/bin/zookeeper-server-start.sh $kconfig/props/zookeeper.properties >> $home/logs/kafka_zookeeper.log 2>&1 &'
+alias kss='nohup $khome/bin/kafka-server-start.sh $kconfig/props/server.properties >> $home/logs/kafka_zookeeper.log 2>&1 &'
+alias kzl=$home/logs/kafka_zookeeper.log
+alias ksl=$home/logs/kafka_zookeeper.log
+
 #sh $lconfig/s.sh
 
