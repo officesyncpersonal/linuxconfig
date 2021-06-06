5be742c48c3edfbe9fa820f99edf8f0fa25ce1b9
DD <dhirajdusanessnc@gmail.com>
2021-06-06 22:33:07 +0530

    Working file

--- s.sh
+++ s.sh
@@ -1,2 +1,11 @@
 #! /bin/sh
+alias s='source $HOME/temp/linuxconfig/s.sh'
 alias c='clear'
+
+export tmp=$HOME/temp
+export lconfig=$tmp/linuxconfig
+export khome=$tmp/kafka
+export kconfig=$tmp/kafkaconfig
+
+#sh $lconfig/s.sh
+
