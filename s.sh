e4f5dbadb29a2c8fdbd4facfaaf1ed20178210fd
DD <dhirajdusanessnc@gmail.com>
2021-06-07 01:06:13 +0530

    few fixes

--- s.sh
+++ s.sh
@@ -6,11 +6,13 @@ export tmp=$HOME/temp
 export lconfig=$tmp/linuxconfig
 export khome=$tmp/kafka
 export kconfig=$tmp/kafkaconfig
+export host=/etc/hosts
 
 alias kzs='nohup $khome/bin/zookeeper-server-start.sh $kconfig/props/zookeeper.properties >> $HOME/logs/kafka_zookeeper.log 2>&1 &'
 alias kss='nohup $khome/bin/kafka-server-start.sh $kconfig/props/server.properties >> $HOME/logs/kafka_server.log 2>&1 &'
-alias kzl=$HOME/logs/kafka_zookeeper.log
-alias ksl=$HOME/logs/kafka_zookeeper.log
+alias kl='cd $HOME/logs'
+
+alias lip='ip addr | grep eth0'
 
 #sh $lconfig/s.sh
 
